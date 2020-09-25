75cd3cb9dbde54363ff04a3f4b3f973b7903451c
ashish <ashish9pt@gmail.com>
2020-09-25 10:57:39 +0530

    i18n First Commit

--- src/main/java/com/example/demo/Hello/HelloWorldController.java
+++ src/main/java/com/example/demo/Hello/HelloWorldController.java
@@ -1,10 +1,19 @@
 package com.example.demo.Hello;
 
+import java.util.Locale;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
+	
+	@Autowired
+	private ResourceBundleMessageSource messageSource;
 	
 	@GetMapping("/helloo")
 	public String helloWorld()
@@ -16,5 +25,17 @@ public class HelloWorldController {
 	public UserDetails hellowWorldBean()
 	{
 		return new UserDetails("Kalyan","Reddy","Panvel");
+	}
+	
+	@GetMapping("/hello-int")
+	public String getMessagesInI18NFormat(@RequestHeader(name="Accept-Langauge", required=false) String locale)
+	{
+		return messageSource.getMessage("label.hello",null, new Locale( locale));
+	}
+	
+	@GetMapping("/hello-int2")
+	public String getMessagesInI18NFormat2()
+	{
+		return messageSource.getMessage("label.hello",null, LocaleContextHolder.getLocale());
 	}
 }
